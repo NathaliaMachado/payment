502055deabaf4f442f32f8519ddc8a026829ff80
NathaliaMachado <nathaliamachd@gmail.com>
2024-11-09 11:48:31 -0300

    Adding service discovery and registry

--- src/main/java/br/com/fastfood/payment/PaymentApplication.java
+++ src/main/java/br/com/fastfood/payment/PaymentApplication.java
@@ -2,8 +2,11 @@ package br.com.fastfood.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentApplication {
 
 	public static void main(String[] args) {
